a4b869b921a6f4f7852740587e57b5c7f8c7fbdc
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2024-07-11 18:25:21 +0900

    initial

---
+++ src/components/buttons/index.tsx
@@ -0,0 +1,24 @@
+import styles from '../style/Button.module.css';
+import React from 'react';
+
+interface ButtonProps {
+  children?: React.ReactNode;
+  size?: 'small' | 'middle' | 'large';
+  color?: 'red' | 'green' | 'blue' | 'white' | 'black';
+  backgroundColor?: 'white' | 'black'; 
+}
+
+const defaultCss = {
+  size: 'middle',
+  color: 'blue',
+  backgroundColor: 'white'
+} satisfies ButtonProps;
+
+const Buttons: React.FC<ButtonProps> = (props) =>{
+  const { children, ...remains } = { ...defaultCss, ...props };
+  const buttonClass = [styles.Button, ...Object.entries(remains).map(([k,v]) => styles[`${k}-${v}`])].join(" ");
+  
+  return <button className={buttonClass}>{children}</button>;
+}
+
+export default Buttons;
